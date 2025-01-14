cf15a122b7c938d07b9fc62d5a792ec9743126ee
adnanmahmud0 <adnan.mahmud.pc@gmail.com>
2025-01-14 18:08:38 +0600

    auth provider setup done

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,13 +1,66 @@
-import { GoogleAuthProvider } from "firebase/auth/web-extension";
-import { createContext, useState } from "react";
+import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import { app } from "../Firebase/firebase.config";
 
 export const AuthContext = createContext(null);
 const AuthProvider = ({children}) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(null);
     const googleProvider = new GoogleAuthProvider();
+    const auth = getAuth(app);
+
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const loginUser = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const logOut = () => {
+        return setLoading(true);
+        return signOut(auth);
+    }
+
+    const signInWithGoogle = () => {
+        return signInWithPopup(auth, googleProvider);
+    }
+
+    const updateUserProfile = (name, photo) => {
+        return updateProfile(auth,currentUser, {
+            displayName: name, photoURL: photo
+        });
+    }
+
+    const passwordReset = (email) => {
+        return sendPasswordResetEmail(auth, email);
+    }
 
 
+
+
+    useEffect( () => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser =>{
+            setUser(currentUser);
+            console.log("current user", currentUser);
+            setLoading(false);
+        });
+        return () => {
+            return unsubscribe();
+        }
+    }, [])
+
+    const authInfo = {
+        user,
+        loading,
+        createUser,
+        loginUser,
+        logOut,
+        signInWithGoogle,
+        updateUserProfile,
+        passwordReset,
+
+    }
 
     return (
         <>
--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,12 @@ import {
 } from "react-router-dom";
 import './index.css'
 import { router } from './Routes/Router';
+import AuthProvider from './Provider/AuthProvider';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>,
 )
