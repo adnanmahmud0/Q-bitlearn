ca95cd62d0157508556095ea0bb7e04deb8fb3b3
adnanmahmud0 <adnan.mahmud.pc@gmail.com>
2025-02-18 22:47:37 +0600

    navebar take all space

--- src/Pages/Root.jsx
+++ src/Pages/Root.jsx
@@ -7,7 +7,9 @@ const Root = () => {
     return (
         <>
             <Navbar></Navbar>
-            <Outlet></Outlet>
+            <div className='pt-20'>
+                <Outlet></Outlet>
+            </div>
             <Footer></Footer>
         </>
     );
